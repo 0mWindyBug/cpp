62b17dcf169137728f3d946613fefa609a219792
Windy Bug <139051196+0mWindyBug@users.noreply.github.com>
2024-03-22 07:55:01 +0000

    Delete InterShell directory

--- InterShell/InterShell/Utils.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <iostream>
-#include <Windows.h>
-
-bool IsRunAsAdmin();
-std::string GetProgramDir();
-const std::string GetFullPath(const std::string& InputPath, const std::string& BasePath);
